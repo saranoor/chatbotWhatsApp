c299040d8fab3f6b9baec990ec7c2c20f2c45340
saranoor <saranoor@live.com>
2026-04-28 11:38:30 +0500

    fix volume_type type issue

--- cdk/storage_stack.py
+++ cdk/storage_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import Stack, aws_opensearchservice as opensearch, RemovalPolicy
 from constructs import Construct
+from aws_cdk import aws_ec2 as ec2
 
 
 class StorageStack(Stack):
@@ -14,6 +15,8 @@ class StorageStack(Stack):
             capacity=opensearch.CapacityConfig(
                 data_node_instance_type="t3.small.search", data_nodes=1
             ),
-            ebs=opensearch.EbsOptions(volume_size=20, volume_type="gp3"),
+            ebs=opensearch.EbsOptions(
+                volume_size=20, volume_type=ec2.EbsDeviceVolumeType.GP3
+            ),
             removal_policy=RemovalPolicy.RETAIN,
         )
