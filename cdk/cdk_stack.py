9d3837a59cf133ee921675d6852cd5d40619bf03
saranoor <saranoor@live.com>
2026-05-08 15:55:43 +0500

    fix domain arn comma

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -72,7 +72,7 @@ class WhatsappBotStack(Stack):
             self,
             "ImportedOSDomain",
             # domain_arn=f"arn:aws:es:{self.region}:{self.accountself.account}:domain/your-domain-name",
-            domain_arn= "arn:aws:es:us-east-1:357457231130:domain/opensearchdomai-0xjhd8to9bbg"
+            domain_arn="arn:aws:es:us-east-1:357457231130:domain/opensearchdomai-0xjhd8to9bbg",
             domain_endpoint="search-opensearchdomai-0xjhd8to9bbg-l6fuyuxgycn47wskwik27wcyiu.us-east-1.es.amazonaws.com",
         )
 
